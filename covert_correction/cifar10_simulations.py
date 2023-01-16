a7db8eb70db77090cbf25c76f091c21f1d98a12f
Chen-Zhong-Jing <82193354+Chen-Zhong-Jing@users.noreply.github.com>
2023-01-16 13:46:47 +0800

    Update cifar10_simulations.py

--- covert_correction/cifar10_simulations.py
+++ covert_correction/cifar10_simulations.py
@@ -21,7 +21,9 @@ from tensorflow import keras
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from models.cifar10_models import build_model
-
+import torch
+from csvec import CSVec
+from pack.dweibull_quantizer import dweibullquantizer
 # Compressions
 # from utils.fp8 import fp8_152_bin_edges
 
@@ -32,24 +34,46 @@ import matplotlib.pyplot as plt
 def parse_args():
     parser = argparse.ArgumentParser(description='Variables for Cifar10 Training')
 
-    'Model Details'
+    # Model Details
     parser.add_argument('--batch-size', type=int, default=64, help='Batch Size for Training and Testing')
     parser.add_argument('--epoch-num', type=int, default=150, help='End Iterations for Training')
     
     parser.add_argument('--optimizer', type=str, default='SGD', help='Optimizer to use. Can be either SGD or Adam')
     parser.add_argument('--learning-rate', type=float, default=0.01, help='Learning Rate for model')
+    parser.add_argument('--lr-decay', type=float, default=0.1, help='Learning Rate decay coefficient')
+    parser.add_argument('--lr-decay-freq', default=50, type=int,help='lr decay frequency in epoch (default: 30)')
     parser.add_argument('--momentum', type=float, default=0, help='Momentum for model')
-    parser.add_argument('--correction-coeff', type=float, default=0.9, help='Correction coefficient for model')
+    parser.add_argument('--model-name', type=str, default='ResNet50V2', help='Model to Load for Training')
     
-    parser.add_argument('--model-name', type=str, default='DenseNet121', help='Model to Load for Training')
+    modes = ["sketch", "CO3", "uncompress", "tinyscript"]
+    parser.add_argument("--mode", choices=modes, default="tinyscript")
     
-    'Print and Plot'
+    # CO3 Details
+    quantizations = ["fp4", "fp8"]
+    parser.add_argument("--quantization", choices=quantizations, default="fp8", help='quantization type from IEEE754')
+    parser.add_argument('--correction-coeff', type=float, default=0.7, help='Correction coefficient for model')
+   
+    # Sketch Details
+    #parser.add_argument("--k", type=int, default=588496)
+    parser.add_argument("--num_cols", type=int, default=1176992)
+    parser.add_argument("--num_rows", type=int, default=5)
+    parser.add_argument("--num_blocks", type=int, default=20)
+    
+    
+    # Tinyscript Details
+    parser.add_argument('--R', type=int, default=8, help='How many bit rate')
+    parser.add_argument('--M', type=int, default=0, help='M for K-means Quantizer')
+    
+    default_device = "cuda" if torch.cuda.is_available() else "cpu"
+    parser.add_argument("--device", type=str, choices=["cpu", "cuda"],default=default_device,help="Device (cuda or cpu)")
+    
+    # Print and Plot
     parser.add_argument('--plot-state', type=str2bool, default='True', help='Whether to plot the results')
     parser.add_argument('--verbose-epoch', type=str2bool, default='True', help='Whether to print the accuracy results on a per batch basis')
     parser.add_argument('--verbose-batch', type=str2bool, default='True', help='Whether to print the accuracy results on a per batch basis')
     
-    'Save Details'
-    parser.add_argument('--filepath', type=str, default='D:/new/best_scalar_error_feedback_gamma0.5/cifar10/', help='Path used for saving the gradients and statistics')
+    # Save Details
+    parser.add_argument('--filepath', type=str, default='D:/Tinyscript/8bit/', help='Path used for saving the gradients and statistics')
     parser.add_argument('--trial-num', type=int, default=0, help='Simulation index')
 
     args = parser.parse_args()
@@ -64,9 +88,14 @@ def run_experiment(args):
     tr_acc = tf.keras.metrics.CategoricalAccuracy()
     #tr_loss = tf.keras.metrics.CategoricalCrossentropy()
     
-    #bin_centers,bin_edges,dict = fp8_152_bin_edges()
-    bin_centers,bin_edges,bin_dict = fp4_121_bin_edges()    #setting of quantization
+    # Setting CO3 Details
+    if args.quantization == 'fp4':
+        bin_centers,bin_edges,bin_dict = fp4_121_bin_edges()
+    elif args.quantization == 'fp8':
+        bin_centers,bin_edges,bin_dict = fp8_152_bin_edges()
     gamma = args.correction_coeff
+    
+    cache_dweibull_quantization_table = {}
     
     # -----------------------------------------------------------------------------  
     "Functions"  
@@ -85,17 +114,9 @@ def run_experiment(args):
         grads_np = []
         quantized_np = []
         grad_plus_error = []
-        if epoch == 0:
-            for idx in range(len(grads)):
-                temp = grads[idx].numpy()
-                grads_np.append(temp.flatten())
-                grad_plus_error.append(temp.flatten())
-                shape = np.shape(temp)
-                quantized_gradient = np.reshape(bin_centers[np.searchsorted(bin_edges, temp.flatten())], shape)
-                quantized_np.append(quantized_gradient)
-                #error[idx] = temp - quantized_gradient
-                grads[idx] = tf.convert_to_tensor(quantized_gradient)
-        else:
+        grads_vec = []
+        grads_size = np.zeros(len(grads),dtype=int)
+        if args.mode == "CO3":
             for idx in range(len(grads)):
                 temp = grads[idx].numpy()
                 grads_np.append(temp.flatten())
@@ -106,6 +127,47 @@ def run_experiment(args):
                 quantized_np.append(quantized_gradient)
                 error[idx] = temp - quantized_gradient
                 grads[idx] = tf.convert_to_tensor(quantized_gradient.astype(dtype=np.float32))
+                    
+        elif args.mode == "sketch":
+            for idx in range(len(grads)):
+                temp = grads[idx].numpy().astype(dtype=np.float32)
+                grads_size[idx] = int(temp.size)
+                grads_vec.append(torch.from_numpy(temp.flatten()))
+
+            grads_vec = torch.cat(grads_vec)
+            grads_vec = grads_vec.to(args.device)
+            sketch = CSVec(d=grads_size.sum(), c=args.num_cols,r=args.num_rows, device=args.device,numBlocks=args.num_blocks)
+            sketch.accumulateVec(grads_vec)
+            """
+            # gradient clipping
+            if compute_grad and args.max_grad_norm is not None:
+                sketch = clip_grad(args.max_grad_norm, sketch)
+            """
+            update = sketch.unSketch(k=grads_size.sum()).cpu().numpy()
+
+            start = 0
+            end = 0
+            for idx in range(len(grads)):
+                end = start + grads_size[idx]
+                grads[idx] = tf.convert_to_tensor(update[start:end].reshape(grads[idx].shape))
+                start = end
+        elif args.mode == "tinyscript":
+            for idx in range(len(grads)):
+                temp = grads[idx].numpy().astype(dtype=np.float32)
+                grads_np.append(temp.flatten())
+                shape = np.shape(temp)
+                temp += gamma*error[idx]
+                grad_plus_error.append(temp.flatten())
+
+                #thresholds, quantization_centers = dweibullquantizer(temp.flatten(), args.R, 100, args.M, cache_dweibull_quantization_table)
+                labels = np.digitize(temp,thresholds[idx])
+                index_labels_false = np.where(labels == 2**args.R)
+                labels[index_labels_false] = 2**args.R - 1
+                quantized_gradient = quantization_centers[idx,labels]
+                quantized_np.append(quantized_gradient.flatten())
+                error[idx] = temp - quantized_gradient
+                grads[idx] = tf.convert_to_tensor(quantized_gradient.astype(dtype=np.float32))
+            
 
         opt.apply_gradients(zip(grads, model.trainable_variables))
         
@@ -173,6 +235,10 @@ def run_experiment(args):
     for idx in range(len(model.trainable_variables)):
         shape = np.shape(model.trainable_variables[idx].numpy())
         quantized_error.append(np.zeros(shape))
+        
+    "Initialize Tinyscript quantization tables"
+    thresholds = np.zeros((len(model.trainable_variables), 2**args.R-1))
+    quantization_centers = np.zeros((len(model.trainable_variables),2**args.R))
     # Initiate the optimizer
     if args.optimizer == 'SGD':
         opt = tf.keras.optimizers.SGD(learning_rate=args.learning_rate,
@@ -202,19 +268,9 @@ def run_experiment(args):
         epoch_time = time.time()
         a0 = np.zeros(batch_total)
         l0 = np.zeros(batch_total)
+        #opt.learning_rate = args.learning_rate * (args.lr_decay ** (epoch // args.lr_decay_freq))
         for batches, (x_batch,y_batch) in enumerate(datagen.flow(x_train, y_train, batch_size=batch_size)):
             grads,l1,quantized_error,quantized_grad, grad_plus_error = step(x_batch,y_batch,quantized_error,scalar)
-    
-            if batches == 0:
-                avr_grads = np.zeros_like(grads)
-                avr_quantized_error = np.zeros_like(quantized_error)
-                avr_quantized_grad = np.zeros_like(quantized_grad)
-                avr_grads_plus_error = np.zeros_like(grad_plus_error)
-                
-            avr_grads += grads
-            avr_quantized_error += quantized_error
-            avr_quantized_grad += quantized_grad
-            avr_grads_plus_error += grad_plus_error
     
             a1 = tr_acc.result().numpy()
             a0[batches] = a1
@@ -223,24 +279,48 @@ def run_experiment(args):
             if args.verbose_batch:
                 print('Batch #', batches, ' Acc:', str(f'{a1:0.4f}'))
             
+
+            if batches == 0:
+                avr_grads = np.zeros_like(grads)
+                avr_quantized_error = np.zeros_like(quantized_error)
+                avr_quantized_grad = np.zeros_like(quantized_grad)
+                avr_grads_plus_error = np.zeros_like(grad_plus_error)
+                
+  
+            avr_grads += grads
+            avr_quantized_error += quantized_error
+            avr_quantized_grad += quantized_grad
+            avr_grads_plus_error += grad_plus_error
+            
             if (batches + 1) >= batch_total:
+                
                 avr_grads_plus_error /= batches
-
+                avr_grads /= batches
+                
                 # Compute the scalar for each layer minimize the L2 loss of fp convertion.
-                if epoch % 5 == 4:
-                    for l in range(len(avr_grads)):
+                if args.mode == "CO3" and epoch % 5 == 4:
+                    for l in range(len(avr_grads_plus_error)):
                         fit_params = stats.gennorm.fit(avr_grads_plus_error[l])
-                        scalar[l] = fp4_best_scalar(fit_params)
-
+                        
+                        if args.quantization == 'fp4':
+                            scalar[l] = fp4_best_scalar(fit_params)
+                        elif args.quantization == 'fp8':
+                            scalar[l] = fp8_best_scalar(fit_params)
+                        
+                if args.mode == "tinyscript" and epoch % 5 == 0:
+                    for l in range(len(avr_grads)):
+                        thresholds[l], quantization_centers[l] = dweibullquantizer(avr_grads[l], args.R, 100, args.M, cache_dweibull_quantization_table)
+                        
                 "Storing the average of each term"
-                data = {'avr_grad': avr_grads / batches,
+                data = {'avr_grad': avr_grads,
                         'avr_quantized_grad': avr_quantized_grad/ batches,
                         'avr_grads_plus_error': avr_grads_plus_error,
                         'quantized_error': avr_quantized_error / batches}
 
                 pickle.dump(data, open(filepath + 'Gradient_epoch' + str(epoch) + '.p', "wb"))
-
+                
                 break
+           
     
         t_loss, t_acc = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
         
@@ -266,6 +346,7 @@ def run_experiment(args):
     
     pickle.dump(data, open(filepath + 'Accuracy_and_loss' + '.p', "wb" ))
     
+    pickle.dump(cache_dweibull_quantization_table, open(filepath + 'dweibull_4bit_quantization_table' + '.p', "wb" ))
     #------------------------------------------------------------------------------
     "Plotting Results"
     if args.plot_state:
